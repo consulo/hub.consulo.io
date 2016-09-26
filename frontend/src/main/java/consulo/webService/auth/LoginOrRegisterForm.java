f7792a315aff3b4eff142f0852c623ed757c5672
VISTALL <vistall.valeriy@gmail.com>
2016-09-26 11:31:47 +0300

    simpilify

--- frontend/src/main/java/consulo/webService/auth/LoginOrRegisterForm.java
+++ frontend/src/main/java/consulo/webService/auth/LoginOrRegisterForm.java
@@ -18,6 +18,10 @@ import com.wcs.wcslib.vaadin.widget.recaptcha.shared.ReCaptchaOptions;
 
 public class LoginOrRegisterForm extends VerticalLayout
 {
+	private ReCaptcha myReCaptcha;
+	private TextField myEmailTextField;
+	private PasswordField myPasswordField;
+
 	public LoginOrRegisterForm(LoginOrRegisterCallback loginCallback, LoginOrRegisterCallback registerCallback)
 	{
 		VerticalLayout mainPanel = new VerticalLayout();
@@ -28,97 +32,33 @@ public class LoginOrRegisterForm extends VerticalLayout
 		addComponent(mainPanel);
 		setComponentAlignment(mainPanel, Alignment.MIDDLE_CENTER);
 
-		TextField email = new TextField("Email");
-		email.setWidth(100, Unit.PERCENTAGE);
-		email.setImmediate(true);
-		email.addValidator(new EmailValidator("Bad email"));
-		mainPanel.addComponent(email);
+		myEmailTextField = new TextField("Email");
+		myEmailTextField.setWidth(100, Unit.PERCENTAGE);
+		myEmailTextField.setImmediate(true);
+		myEmailTextField.addValidator(new EmailValidator("Bad email"));
+		//myEmailTextField.setValidationVisible(true);
+		mainPanel.addComponent(myEmailTextField);
 
-		PasswordField password = new PasswordField("Password");
-		password.setWidth(100, Unit.PERCENTAGE);
-		password.addValidator(new StringLengthValidator("Bad password", 1, Integer.MAX_VALUE, false));
-		mainPanel.addComponent(password);
-
+		myPasswordField = new PasswordField("Password");
+		myPasswordField.setWidth(100, Unit.PERCENTAGE);
+		//myPasswordField.setValidationVisible(true);
+		myPasswordField.addValidator(new StringLengthValidator("Bad password", 1, Integer.MAX_VALUE, false));
+		mainPanel.addComponent(myPasswordField);
 
 		ReCaptchaOptions reCaptchaOptions = new ReCaptchaOptions();
 		reCaptchaOptions.type = "image";
 		reCaptchaOptions.theme = "light";
 		reCaptchaOptions.sitekey = "6LeUWtwSAAAAAM8RcPWKcjKjTroM8K7iK0Oikh6l";
 
-		ReCaptcha reCaptcha = new ReCaptcha("6LeUWtwSAAAAAMN_ao4CuJfC8sh1sWeh0rQPftbQ", reCaptchaOptions);
+		myReCaptcha = new ReCaptcha("6LeUWtwSAAAAAMN_ao4CuJfC8sh1sWeh0rQPftbQ", reCaptchaOptions);
 
-		Button loginButton = new Button("Login", evt -> {
-			if(!email.isValid() || !password.isValid())
-			{
-				return;
-			}
-
-			if(!reCaptcha.validate())
-			{
-				if(!reCaptcha.isEmpty())
-				{
-					reCaptcha.reload();
-				}
-
-				Notification notification = new Notification("ReCaptcha is bad", Notification.Type.ERROR_MESSAGE);
-				notification.setPosition(Position.TOP_RIGHT);
-				notification.setDelayMsec(3000);
-				notification.show(Page.getCurrent());
-				return;
-			}
-
-			if(!loginCallback.loginAndPasswordEntered(email.getValue(), password.getValue()))
-			{
-				if(!reCaptcha.isEmpty())
-				{
-					reCaptcha.reload();
-				}
-
-				Notification notification = new Notification("Login failed", Notification.Type.ERROR_MESSAGE);
-				notification.setPosition(Position.TOP_RIGHT);
-				notification.setDelayMsec(3000);
-				notification.show(Page.getCurrent());
-			}
-		});
+		Button loginButton = new Button("Login", createListener("Login failed", loginCallback));
 		loginButton.addStyleName(ValoTheme.BUTTON_PRIMARY);
 		loginButton.setClickShortcut(ShortcutAction.KeyCode.ENTER);
 
-		Button registerButton = new Button("Register", evt -> {
-			if(!email.isValid() || !password.isValid())
-			{
-				return;
-			}
+		Button registerButton = new Button("Register", createListener("Register failed", registerCallback));
 
-			if(!reCaptcha.validate())
-			{
-				if(!reCaptcha.isEmpty())
-				{
-					reCaptcha.reload();
-				}
-
-				Notification notification = new Notification("ReCaptcha is bad", Notification.Type.ERROR_MESSAGE);
-				notification.setPosition(Position.TOP_RIGHT);
-				notification.setDelayMsec(3000);
-				notification.show(Page.getCurrent());
-				return;
-			}
-
-			if(!registerCallback.loginAndPasswordEntered(email.getValue(), password.getValue()))
-			{
-				if(!reCaptcha.isEmpty())
-				{
-					reCaptcha.reload();
-				}
-
-				Notification notification = new Notification("Register failed", Notification.Type.ERROR_MESSAGE);
-				notification.setPosition(Position.TOP_RIGHT);
-				notification.setDelayMsec(3000);
-				notification.show(Page.getCurrent());
-			}
-		});
-
-
-		mainPanel.addComponent(reCaptcha);
+		mainPanel.addComponent(myReCaptcha);
 
 		HorizontalLayout buttonsPanel = new HorizontalLayout(loginButton, registerButton);
 		buttonsPanel.setComponentAlignment(registerButton, Alignment.MIDDLE_RIGHT);
@@ -126,6 +66,47 @@ public class LoginOrRegisterForm extends VerticalLayout
 		buttonsPanel.setSpacing(true);
 
 		mainPanel.addComponent(buttonsPanel);
+	}
+
+	private Button.ClickListener createListener(String error, LoginOrRegisterCallback callback)
+	{
+		return event -> {
+			if(!myReCaptcha.validate())
+			{
+				Notification notification = new Notification("ReCaptcha is bad", Notification.Type.ERROR_MESSAGE);
+				notification.setPosition(Position.TOP_RIGHT);
+				notification.setDelayMsec(3000);
+				notification.show(Page.getCurrent());
+				return;
+			}
+
+			clearReCaptcha();
+
+			if(!myEmailTextField.isValid() || !myPasswordField.isValid())
+			{
+				Notification notification = new Notification("Email or password is bad", Notification.Type.ERROR_MESSAGE);
+				notification.setPosition(Position.TOP_RIGHT);
+				notification.setDelayMsec(3000);
+				notification.show(Page.getCurrent());
+				return;
+			}
+
+			if(!callback.loginAndPasswordEntered(myEmailTextField.getValue(), myPasswordField.getValue()))
+			{
+				Notification notification = new Notification(error, Notification.Type.ERROR_MESSAGE);
+				notification.setPosition(Position.TOP_RIGHT);
+				notification.setDelayMsec(3000);
+				notification.show(Page.getCurrent());
+			}
+		};
+	}
+
+	private void clearReCaptcha()
+	{
+		if(!myReCaptcha.isEmpty())
+		{
+			myReCaptcha.reload();
+		}
 	}
 
 	@FunctionalInterface
