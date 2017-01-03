11ab20a62039b8614522176b814829afcdca4c0a
VISTALL <vistall.valeriy@gmail.com>
2017-01-03 10:35:58 +0300

    preparing

---
+++ frontend/src/main/java/consulo/webService/plugins/archive/TarGzArchive.java
@@ -0,0 +1,207 @@
+package consulo.webService.plugins.archive;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveException;
+import org.apache.commons.compress.archivers.ArchiveOutputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.compress.archivers.tar.TarConstants;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
+import org.apache.commons.compress.utils.IOUtils;
+import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.util.io.FileUtilRt;
+import com.intellij.openapi.util.io.StreamUtil;
+import com.intellij.util.io.tar.TarEntry;
+
+/**
+ * @author VISTALL
+ * @since 02-Jan-17
+ */
+public class TarGzArchive
+{
+	private final Map<String, TarGzArchiveEntry> myEntries = new LinkedHashMap<>();
+	private final File myFile;
+
+	public TarGzArchive(File file)
+	{
+		myFile = file;
+	}
+
+	public void prepare(@NotNull File targetDirectory) throws IOException
+	{
+		FileUtilRt.delete(targetDirectory);
+
+		try (TarArchiveInputStream ais = new TarArchiveInputStream(new GzipCompressorInputStream(new FileInputStream(myFile))))
+		{
+			TarArchiveEntry tempEntry;
+			while((tempEntry = (TarArchiveEntry) ais.getNextEntry()) != null)
+			{
+				String name = tempEntry.getName();
+				File targetFile = new File(targetDirectory, name);
+
+				byte flags;
+				if(tempEntry.isDirectory())
+				{
+					flags = TarConstants.LF_DIR;
+				}
+				else if(tempEntry.isSymbolicLink())
+				{
+					flags = TarConstants.LF_SYMLINK;
+				}
+				else if(tempEntry.isLink())
+				{
+					flags = TarConstants.LF_LINK;
+				}
+				else
+				{
+					flags = TarConstants.LF_NORMAL;
+				}
+
+				TarGzArchiveEntry value = new TarGzArchiveEntry(name, tempEntry.isDirectory(), tempEntry.getMode(), tempEntry.getLastModifiedDate().getTime(), flags, tempEntry.getLinkName());
+				if(tempEntry.isDirectory())
+				{
+					FileUtilRt.createDirectory(targetFile);
+				}
+				else
+				{
+					try (OutputStream stream = new FileOutputStream(targetFile))
+					{
+						StreamUtil.copyStreamContent(ais, stream);
+					}
+
+					value.setExtractedFile(targetFile, tempEntry.getSize());
+				}
+
+				myEntries.put(name, value);
+			}
+		}
+	}
+
+	public void create(@NotNull File file, @NotNull String type) throws IOException, ArchiveException
+	{
+		ArchiveStreamFactory factory = new ArchiveStreamFactory();
+		try (OutputStream pathStream = createFileStream(file, type))
+		{
+			ArchiveOutputStream archiveOutputStream = factory.createArchiveOutputStream(type, pathStream);
+			if(archiveOutputStream instanceof TarArchiveOutputStream)
+			{
+				((TarArchiveOutputStream) archiveOutputStream).setLongFileMode(TarArchiveOutputStream.LONGFILE_GNU);
+			}
+
+			for(Map.Entry<String, TarGzArchiveEntry> entry : myEntries.entrySet())
+			{
+				TarGzArchiveEntry value = entry.getValue();
+
+				ArchiveEntry archiveEntry = createEntry(entry.getKey(), value, type);
+
+				if(!value.isDirectory())
+				{
+					setEntrySize(value, archiveEntry);
+
+					archiveOutputStream.putArchiveEntry(archiveEntry);
+
+					byte[] extractedData = value.getExtractedData();
+					if(extractedData != null)
+					{
+						try (ByteArrayInputStream fileInputStream = new ByteArrayInputStream(extractedData))
+						{
+							IOUtils.copy(fileInputStream, archiveOutputStream);
+						}
+					}
+					else
+					{
+						try (FileInputStream fileInputStream = new FileInputStream(value.getExtractedFile()))
+						{
+							IOUtils.copy(fileInputStream, archiveOutputStream);
+						}
+					}
+
+					archiveOutputStream.closeArchiveEntry();
+				}
+			}
+
+			archiveOutputStream.finish();
+		}
+	}
+
+	private void setEntrySize(TarGzArchiveEntry value, ArchiveEntry archiveEntry)
+	{
+		if(archiveEntry instanceof ZipArchiveEntry)
+		{
+			((ZipArchiveEntry) archiveEntry).setSize(value.getSize());
+		}
+		else if(archiveEntry instanceof TarArchiveEntry)
+		{
+			((TarArchiveEntry) archiveEntry).setSize(value.getSize());
+		}
+		else
+		{
+			throw new IllegalArgumentException(archiveEntry.getClass().getName());
+		}
+	}
+
+	@NotNull
+	private ArchiveEntry createEntry(String name, TarGzArchiveEntry entry, String type)
+	{
+		ArchiveEntry archiveEntry;
+		switch(type)
+		{
+			case ArchiveStreamFactory.TAR:
+				TarArchiveEntry tarArchiveEntry = new TarArchiveEntry(name, entry.getFlags());
+				String linkName = entry.getLinkName();
+				if(linkName != null)
+				{
+					tarArchiveEntry.setLinkName(linkName);
+				}
+				tarArchiveEntry.setMode(entry.getMode());
+				tarArchiveEntry.setModTime(entry.getTime());
+
+				archiveEntry = tarArchiveEntry;
+				break;
+			case ArchiveStreamFactory.ZIP:
+				ZipArchiveEntry zipArchiveEntry = new ZipArchiveEntry(name);
+				zipArchiveEntry.setTime(entry.getTime());
+				archiveEntry = zipArchiveEntry;
+				break;
+			default:
+				throw new IllegalArgumentException(type);
+		}
+		return archiveEntry;
+	}
+
+	public boolean removeEntry(@NotNull String entryName)
+	{
+		return myEntries.remove(entryName) != null;
+	}
+
+	public void putEntry(@NotNull String entryName, @NotNull byte[] data)
+	{
+		TarGzArchiveEntry entry = new TarGzArchiveEntry(entryName, false, TarEntry.DEFAULT_FILE_MODE, System.currentTimeMillis(), TarEntry.LF_NORMAL, null);
+		entry.setExtractedData(data);
+		myEntries.put(entryName, entry);
+	}
+
+	@NotNull
+	private static OutputStream createFileStream(File file, String type) throws IOException
+	{
+		FileOutputStream fileOutputStream = new FileOutputStream(file);
+		if(ArchiveStreamFactory.TAR.equals(type))
+		{
+			return new GzipCompressorOutputStream(fileOutputStream);
+		}
+		return fileOutputStream;
+	}
+}
---
+++ frontend/src/main/java/consulo/webService/plugins/archive/TarGzArchiveEntry.java
@@ -0,0 +1,89 @@
+package consulo.webService.plugins.archive;
+
+import java.io.File;
+
+/**
+ * @author VISTALL
+ * @since 02-Jan-17
+ */
+public class TarGzArchiveEntry
+{
+	private String myName;
+	private boolean myDirectory;
+	private int myMode;
+	private long myTime;
+	private byte myFlags;
+	private String myLinkName;
+
+	private File myExtractedFile;
+	private byte[] myExtractedData;
+
+	private long mySize = -1;
+
+	public TarGzArchiveEntry(String name, boolean directory, int mode, long time, byte flags, String linkName)
+	{
+		myName = name;
+		myDirectory = directory;
+		myMode = mode;
+		myTime = time;
+		myFlags = flags;
+		myLinkName = linkName;
+	}
+
+	public byte getFlags()
+	{
+		return myFlags;
+	}
+
+	public String getLinkName()
+	{
+		return myLinkName;
+	}
+
+	public void setExtractedData(byte[] extractedData)
+	{
+		myExtractedData = extractedData;
+		mySize = extractedData.length;
+	}
+
+	public void setExtractedFile(File extractedFile, long size)
+	{
+		myExtractedFile = extractedFile;
+		mySize = size;
+	}
+
+	public String getName()
+	{
+		return myName;
+	}
+
+	public boolean isDirectory()
+	{
+		return myDirectory;
+	}
+
+	public int getMode()
+	{
+		return myMode;
+	}
+
+	public long getTime()
+	{
+		return myTime;
+	}
+
+	public long getSize()
+	{
+		return mySize;
+	}
+
+	public byte[] getExtractedData()
+	{
+		return myExtractedData;
+	}
+
+	public File getExtractedFile()
+	{
+		return myExtractedFile;
+	}
+}
