7c2ac129b6c180df2b6b6797cbf17e94c165635e
VISTALL <vistall.valeriy@gmail.com>
2017-01-25 21:35:29 +0300

    preparing

--- frontend/src/main/java/consulo/webService/auth/view/DashboardView.java
+++ frontend/src/main/java/consulo/webService/auth/view/DashboardView.java
@@ -3,8 +3,13 @@ package consulo.webService.auth.view;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.VerticalSplitPanel;
+import consulo.webService.ui.util.TidyComponents;
 
 @SpringView(name = DashboardView.ID)
 public class DashboardView extends VerticalLayout implements View
@@ -14,7 +19,62 @@ public class DashboardView extends VerticalLayout implements View
 	public DashboardView()
 	{
 		setMargin(true);
+		setSizeFull();
 		addComponent(new Label("Dashboard"));
+
+		VerticalSplitPanel panel = new VerticalSplitPanel();
+		panel.setSizeFull();
+		panel.setSplitPosition(50, Unit.PERCENTAGE);
+
+		addComponent(panel);
+		setExpandRatio(panel, 0.9f);
+
+		HorizontalLayout topLayout = new HorizontalLayout();
+		topLayout.setSizeFull();
+		panel.setFirstComponent(topLayout);
+
+		HorizontalLayout bottomLayout = new HorizontalLayout();
+		bottomLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+		bottomLayout.setSizeFull();
+
+		bottomLayout.addComponent(buildLastPluginComments());
+		bottomLayout.addComponent(buildLastSettingsUpdate());
+		bottomLayout.addComponent(buildLastErrorReports());
+
+		panel.setSecondComponent(bottomLayout);
+	}
+
+	private Component buildLastPluginComments()
+	{
+		VerticalLayout verticalLayout = new VerticalLayout();
+		verticalLayout.setHeight(100, Unit.PERCENTAGE);
+		verticalLayout.setWidth(20, Unit.EM);
+
+		verticalLayout.addComponent(new Label("Last Plugin Comments:"));
+		verticalLayout.addComponent(TidyComponents.newLabel("TODO"));
+		return verticalLayout;
+	}
+
+	private Component buildLastSettingsUpdate()
+	{
+		VerticalLayout verticalLayout = new VerticalLayout();
+		verticalLayout.setHeight(100, Unit.PERCENTAGE);
+		verticalLayout.setWidth(20, Unit.EM);
+
+		verticalLayout.addComponent(new Label("Last Settigs Update:"));
+		verticalLayout.addComponent(TidyComponents.newLabel("TODO"));
+		return verticalLayout;
+	}
+
+	private Component buildLastErrorReports()
+	{
+		VerticalLayout verticalLayout = new VerticalLayout();
+		verticalLayout.setHeight(100, Unit.PERCENTAGE);
+		verticalLayout.setWidth(20, Unit.EM);
+
+		verticalLayout.addComponent(new Label("Last Error Reports:"));
+		verticalLayout.addComponent(TidyComponents.newLabel("TODO"));
+		return verticalLayout;
 	}
 
 	@Override
