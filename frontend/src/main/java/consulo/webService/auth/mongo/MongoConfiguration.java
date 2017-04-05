0371376a7cc0042e2a058e2631bbc19b27a9e34e
VISTALL <vistall.valeriy@gmail.com>
2017-04-05 08:13:44 +0300

    working on mongo auth

--- frontend/src/main/java/consulo/webService/auth/mongo/MongoConfiguration.java
+++ frontend/src/main/java/consulo/webService/auth/mongo/MongoConfiguration.java
@@ -1,5 +1,9 @@
 package consulo.webService.auth.mongo;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import com.mongodb.Mongo;
@@ -21,6 +25,14 @@ public class MongoConfiguration extends AbstractMongoConfiguration
 	@Override
 	public Mongo mongo() throws Exception
 	{
-		return new MongoClient("localhost");
+		try
+		{
+			Context initCtx = new InitialContext();
+			return (MongoClient) initCtx.lookup("java:/comp/env/mongodb/hub");
+		}
+		catch(NamingException e)
+		{
+			return new MongoClient("localhost");
+		}
 	}
 }
---
+++ frontend/src/main/java/org/mongodb/datasource/MongoDatasource.java
@@ -0,0 +1,40 @@
+package org.mongodb.datasource;
+
+import com.mongodb.MongoClient;
+
+/**
+ * https://github.com/pozil/mongodb-jndi-datasource
+ *
+ * Bean holding an open MongoClient used as a pooled datasource and its configuration
+ *
+ * @author Philippe Ozil
+ */
+public class MongoDatasource
+{
+	private final MongoClient client;
+	private final MongoDatasourceConfiguration config;
+
+	protected MongoDatasource(final MongoClient client, final MongoDatasourceConfiguration config)
+	{
+		this.client = client;
+		this.config = config;
+	}
+
+	/**
+	 * Retrieves a client from this datasource
+	 *
+	 * @return DB object representing a MongoDB connection
+	 */
+	public MongoClient getClient()
+	{
+		return client;
+	}
+
+	/**
+	 * Closes all datasource connections
+	 */
+	public void close()
+	{
+		client.close();
+	}
+}
