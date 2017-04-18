6eb521b067551a59ff15007b8a48fb8ab2a0700a
VISTALL <vistall.valeriy@gmail.com>
2017-04-18 17:00:11 +0300

    not implemented yet

--- frontend/src/main/java/consulo/webService/auth/view/UserInfoView.java
+++ frontend/src/main/java/consulo/webService/auth/view/UserInfoView.java
@@ -1,10 +1,13 @@
 package consulo.webService.auth.view;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
+import consulo.webService.ui.util.TidyComponents;
 
 /**
  * @author VISTALL
@@ -17,8 +20,7 @@ public class UserInfoView extends VerticalLayout implements View
 
 	public UserInfoView()
 	{
-		setMargin(true);
-
+		setSizeFull();
 	}
 
 	@Override
@@ -26,6 +28,23 @@ public class UserInfoView extends VerticalLayout implements View
 	{
 		removeAllComponents();
 
-		addComponent(new Label("User Info"));
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if(authentication == null)
+		{
+			return;
+		}
+
+		Label label = new Label("Profile");
+		label.addStyleName("headerMargin");
+		addComponent(label);
+
+		VerticalLayout verticalLayout = new VerticalLayout();
+		verticalLayout.setSizeFull();
+		verticalLayout.addStyleName("bodyMargin");
+
+		verticalLayout.addComponent(TidyComponents.newLabel("Not Implemented Yet"));
+
+		addComponent(verticalLayout);
+		setExpandRatio(verticalLayout, 1f);
 	}
 }
