a6cf38cac02878113ed0e429cfbae5ca56bd36d2
VISTALL <vistall.valeriy@gmail.com>
2017-04-25 18:48:14 +0300

    footer

--- frontend/src/main/java/consulo/webService/ui/RootContentView.java
+++ frontend/src/main/java/consulo/webService/ui/RootContentView.java
@@ -1,9 +1,18 @@
 package consulo.webService.ui;
 
+import java.time.LocalDate;
+
 import org.jetbrains.annotations.NotNull;
+import com.vaadin.server.ExternalResource;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.ComponentContainer;
 import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Link;
+import com.vaadin.ui.themes.ValoTheme;
 
 /**
  * @author VISTALL
@@ -23,8 +32,41 @@ public class RootContentView extends HorizontalLayout
 		myComponentContainer = new CssLayout();
 		myComponentContainer.addStyleName("view-content");
 		myComponentContainer.setSizeFull();
-		addComponent(myComponentContainer);
-		setExpandRatio(myComponentContainer, 1.0f);
+
+		GridLayout table = new GridLayout(1, 2);
+		table.setSizeFull();
+
+		table.addComponent(myComponentContainer, 0, 0);
+
+		HorizontalLayout bottomPanel = new HorizontalLayout();
+		bottomPanel.setWidth(100, Unit.PERCENTAGE);
+		bottomPanel.addStyleName("bottomPanel");
+		bottomPanel.setSpacing(true);
+
+		HorizontalLayout leftLayout = new HorizontalLayout();
+		Label copyright = new Label(String.format("@ %d consulo.io", LocalDate.now().getYear()));
+		copyright.addStyleName(ValoTheme.LABEL_SMALL);
+		copyright.addStyleName(ValoTheme.LABEL_LIGHT);
+		leftLayout.addComponent(copyright);
+
+		bottomPanel.addComponent(leftLayout);
+		bottomPanel.setComponentAlignment(leftLayout, Alignment.MIDDLE_LEFT);
+
+		HorizontalLayout rightLayout = new HorizontalLayout();
+		Link link = new Link("hub.consulo.io", new ExternalResource("https://github.com/consulo/hub.consulo.io"));
+		link.addStyleName(ValoTheme.LINK_SMALL);
+		link.setIcon(FontAwesome.GITHUB);
+		rightLayout.addComponent(link);
+
+		bottomPanel.addComponent(rightLayout);
+		bottomPanel.setComponentAlignment(rightLayout, Alignment.MIDDLE_RIGHT);
+
+		table.addComponent(bottomPanel, 0, 1);
+
+		table.setRowExpandRatio(0, 1f);
+
+		addComponent(table);
+		setExpandRatio(table, 1.0f);
 	}
 
 	@NotNull
