b6a13af941f981a08bd93b88bb6eaace8a6639a2
VISTALL <vistall.valeriy@gmail.com>
2017-06-07 09:42:04 +0300

    not implemented yet

--- frontend/src/main/java/consulo/webService/errorReporter/view/ErrorStatisticsView.java
+++ frontend/src/main/java/consulo/webService/errorReporter/view/ErrorStatisticsView.java
@@ -3,7 +3,10 @@ package consulo.webService.errorReporter.view;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
+import consulo.webService.ui.util.TinyComponents;
 
 /**
  * @author VISTALL
@@ -12,11 +15,25 @@ import com.vaadin.ui.VerticalLayout;
 @SpringView(name = ErrorStatisticsView.ID)
 public class ErrorStatisticsView extends VerticalLayout implements View
 {
-	public static final String ID = "errorStatistics";
+	public static final String ID = "errorReportStatistics";
 
 	@Override
 	public void enter(ViewChangeListener.ViewChangeEvent event)
 	{
+		removeAllComponents();
 
+		HorizontalLayout header = new HorizontalLayout();
+		header.addStyleName("headerMargin");
+		header.setWidth(100, Unit.PERCENTAGE);
+		Label myLabel = new Label("Error Report Statistics");
+
+		header.addComponent(myLabel);
+
+		HorizontalLayout layout = new HorizontalLayout();
+		layout.addComponent(TinyComponents.newLabel("Not Implemented Yet"));
+		layout.addStyleName("bodyMargin");
+		addComponent(layout);
+
+		setExpandRatio(layout, 1);
 	}
 }
