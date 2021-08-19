7f88f56b07f8ad87814ae9aa965f374094d3f81e
VISTALL <vistall.valeriy@gmail.com>
2021-08-19 21:29:04 +0300

    add getAll method

--- frontend/src/main/java/consulo/webService/storage/StorageRestController.java
+++ frontend/src/main/java/consulo/webService/storage/StorageRestController.java
@@ -1,6 +1,7 @@
 package consulo.webService.storage;
 
 import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.util.io.BufferExposingByteArrayOutputStream;
 import consulo.externalStorage.storage.DataCompressor;
 import consulo.webService.auth.oauth2.domain.OAuth2AuthenticationAccessToken;
 import consulo.webService.auth.oauth2.mongo.OAuth2AccessTokenRepository;
@@ -10,6 +11,8 @@ import consulo.webService.storage.bean.PushFileBeanResponse;
 import consulo.webService.storage.mongo.MongoStorageFile;
 import consulo.webService.storage.mongo.MongoStorageFileRepository;
 import consulo.webService.storage.mongo.MongoStorageFileUpdateBy;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.List;
 
@@ -40,7 +44,7 @@ public class StorageRestController
 	private OAuth2AccessTokenRepository myOAuth2AccessTokenRepository;
 
 	@RequestMapping(value = "/api/storage/infoAll", method = RequestMethod.GET)
-	public InfoAllBeanResponse getAll(@RequestHeader("Authorization") String authorization) throws IOException
+	public InfoAllBeanResponse infoAll(@RequestHeader("Authorization") String authorization) throws IOException
 	{
 		OAuth2AuthenticationAccessToken token = myOAuth2AccessTokenRepository.findByTokenId(authorization);
 		if(token == null)
@@ -59,39 +63,50 @@ public class StorageRestController
 		return response;
 	}
 
-	//	@RequestMapping(value = "/api/storage/getAll", method = RequestMethod.GET)
-	//	public ResponseEntity<?> getAll(@RequestHeader("Authorization") String authorization) throws IOException
-	//	{
-	//		OAuth2AuthenticationAccessToken token = myOAuth2AccessTokenRepository.findByTokenId(authorization);
-	//		if(token == null)
-	//		{
-	//			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-	//		}
-	//
-	//		List<MongoStorageFile> list = myStorageFileRepository.findByEmail(token.getUserName());
-	//		if(list.isEmpty())
-	//		{
-	//			return ResponseEntity.noContent().build();
-	//		}
-	//
-	//		BufferExposingByteArrayOutputStream arrayOutputStream = new BufferExposingByteArrayOutputStream();
-	//		try (DataOutputStream stream = new DataOutputStream(arrayOutputStream))
-	//		{
-	//			stream.writeByte(0x01); // version
-	//
-	//			stream.writeShort(list.size());
-	//			for(MongoStorageFile storageFile : list)
-	//			{
-	//				stream.writeInt(storageFile.getModCount());
-	//				stream.writeUTF(storageFile.getFilePath());
-	//				byte[] data = storageFile.getData();
-	//				stream.writeShort(data.length);
-	//				stream.write(data);
-	//			}
-	//		}
-	//
-	//		return ResponseEntity.ok(new ByteArrayResource(arrayOutputStream.toByteArray()));
-	//	}
+	@RequestMapping(value = "/api/storage/getAll", method = RequestMethod.GET)
+	public ResponseEntity<?> getAll(@RequestHeader("Authorization") String authorization) throws IOException
+	{
+		OAuth2AuthenticationAccessToken token = myOAuth2AccessTokenRepository.findByTokenId(authorization);
+		if(token == null)
+		{
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		}
+
+		List<MongoStorageFile> list = myStorageFileRepository.findByEmail(token.getUserName());
+		if(list.isEmpty())
+		{
+			return ResponseEntity.noContent().build();
+		}
+
+		BufferExposingByteArrayOutputStream outputStream = new BufferExposingByteArrayOutputStream();
+
+		try(ZipArchiveOutputStream zipStream = new ZipArchiveOutputStream(outputStream))
+		{
+			zipStream.setMethod(ZipArchiveOutputStream.DEFLATED);
+			zipStream.setEncoding(StandardCharsets.UTF_8.toString());
+
+			for(MongoStorageFile storageFile : list)
+			{
+				String path = storageFile.getFilePath().replace("\\", "/");
+
+				ZipArchiveEntry entry = new ZipArchiveEntry(path);
+
+				zipStream.putArchiveEntry(entry);
+				zipStream.write(storageFile.getData());
+				zipStream.closeArchiveEntry();
+
+				entry = new ZipArchiveEntry(path + ".modcount");
+				zipStream.putArchiveEntry(entry);
+				zipStream.write(String.valueOf(storageFile.getModCount()).getBytes(StandardCharsets.UTF_8));
+				zipStream.closeArchiveEntry();
+			}
+
+			zipStream.finish();
+		}
+
+		byte[] bytes = outputStream.toByteArray();
+		return ResponseEntity.status(HttpStatus.OK).contentLength(bytes.length).body(bytes);
+	}
 
 	@RequestMapping(value = "/api/storage/getFile", method = RequestMethod.GET)
 	public ResponseEntity<?> getFile(@RequestParam("filePath") String filePath, @RequestParam("modCount") int modCount, @RequestHeader("Authorization") String authorization)
