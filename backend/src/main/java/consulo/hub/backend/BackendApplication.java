a4c0ddab9579165e81f021eacc0f88fdb2b676df
VISTALL <vistall.valeriy@gmail.com>
2021-08-21 01:00:16 +0300

    fix running backendservice

--- backend/src/main/java/consulo/hub/backend/BackendApplication.java
+++ backend/src/main/java/consulo/hub/backend/BackendApplication.java
@@ -1,13 +1,118 @@
 package consulo.hub.backend;
 
-/**
- * @author VISTALL
- * @since 20/08/2021
- */
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import consulo.hub.backend.auth.oauth2.OAuth2LoginAuthenticationProvider;
+import consulo.hub.backend.auth.service.LocalAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.MultipartConfigElement;
+
+@EnableScheduling
+@EnableJpaRepositories
+@SpringBootApplication(exclude = {
+		WebSocketAutoConfiguration.class,
+		SecurityAutoConfiguration.class
+})
+@EntityScan("consulo.hub.shared.*")
 public class BackendApplication
 {
+	@Configuration
+	public static class Setup
+	{
+		@Autowired
+		private ObjectMapper objectMapper;
+
+		@Bean
+		public StandardServletMultipartResolver multipartResolver()
+		{
+			return new StandardServletMultipartResolver();
+		}
+
+		@Bean
+		public MultipartConfigElement multipartConfigElement()
+		{
+			int _256mb = 256 * 1024 * 1024;
+			return new MultipartConfigElement(null, _256mb, _256mb, -1);
+		}
+
+		@Bean
+		public PasswordEncoder passwordEncoder()
+		{
+			return new BCryptPasswordEncoder();
+		}
+
+		@Bean
+		public TaskExecutor taskExecutor()
+		{
+			return new ThreadPoolTaskExecutor();
+		}
+
+		@Bean
+		public TaskScheduler taskScheduler()
+		{
+			return new ThreadPoolTaskScheduler();
+		}
+
+		@PostConstruct
+		public void setup()
+		{
+			objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+		}
+	}
+
+	@Configuration
+	@EnableGlobalMethodSecurity(securedEnabled = true)
+	public static class SecurityConfiguration extends GlobalMethodSecurityConfiguration
+	{
+		@Autowired
+		private LocalAuthenticationProvider myLocalAuthenticationProvider;
+
+		@Autowired
+		private UserDetailsService myUserDetailsService;
+
+		@Autowired
+		private TokenStore myTokenStore;
+
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception
+		{
+			auth.authenticationProvider(myLocalAuthenticationProvider);
+			auth.authenticationProvider(new OAuth2LoginAuthenticationProvider(myUserDetailsService, myTokenStore));
+		}
+
+		@Bean
+		public AuthenticationManager authenticationManagerBean() throws Exception
+		{
+			return authenticationManager();
+		}
+	}
+
 	public static void main(String[] args)
 	{
-
+		SpringApplication.run(BackendApplication.class, args);
 	}
 }
