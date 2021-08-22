8b0ea63498e241f32b854b00b5785b500fecf131
VISTALL <vistall.valeriy@gmail.com>
2021-08-22 19:21:41 +0300

    drop old mongo configuration

---
+++ backend/src/main/java/consulo/hub/backend/configuration/MongoConfiguration.java
@@ -0,0 +1,30 @@
+package consulo.hub.backend.configuration;
+
+import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+/**
+ * @author VISTALL
+ * @since 22/08/2021
+ */
+@Configuration
+public class MongoConfiguration
+{
+	@Value("${mongo.host:localhost}")
+	private String myMongoHostName;
+
+	@Bean
+	public MongoClient mongo()
+	{
+		return new MongoClient(myMongoHostName);
+	}
+
+	@Bean
+	public MongoTemplate mongoTemplate() throws Exception
+	{
+		return new MongoTemplate(mongo(), "auth");
+	}
+}
