cec7f45d07a496258497cb59c7e66d78e69d3278
VISTALL <vistall.valeriy@gmail.com>
2021-08-22 17:17:37 +0300

    drop usage of context since we running not inside tomcat anymore

--- backend/src/main/java/consulo/hub/backend/auth/mongo/MongoConfiguration.java
+++ backend/src/main/java/consulo/hub/backend/auth/mongo/MongoConfiguration.java
@@ -2,12 +2,9 @@ package consulo.hub.backend.auth.mongo;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 /**
  * @author VISTALL
@@ -16,6 +13,9 @@ import javax.naming.NamingException;
 @Configuration
 public class MongoConfiguration extends AbstractMongoConfiguration
 {
+	@Value("${mongo.host:localhost}")
+	private String myMongoHostName;
+
 	@Override
 	protected String getDatabaseName()
 	{
@@ -25,14 +25,6 @@ public class MongoConfiguration extends AbstractMongoConfiguration
 	@Override
 	public Mongo mongo() throws Exception
 	{
-		try
-		{
-			Context initCtx = new InitialContext();
-			return (MongoClient) initCtx.lookup("java:/comp/env/mongodb/hub");
-		}
-		catch(NamingException e)
-		{
-			return new MongoClient("localhost");
-		}
+		return new MongoClient(myMongoHostName);
 	}
 }
