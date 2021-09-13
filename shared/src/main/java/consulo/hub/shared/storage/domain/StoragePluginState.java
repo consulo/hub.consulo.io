2be116fb16435010f1c5638e0b1db52765e7eacd
VISTALL <vistall.valeriy@gmail.com>
2021-09-13 23:32:46 +0300

    code style

--- shared/src/main/java/consulo/hub/shared/storage/domain/StoragePluginState.java
+++ shared/src/main/java/consulo/hub/shared/storage/domain/StoragePluginState.java
@@ -4,8 +4,9 @@ package consulo.hub.shared.storage.domain;
  * @author VISTALL
  * @since 13/09/2021
  */
-public enum StoragePluginState {
-    ENABLED,
-    DISABLED,
-    UNINSTALLED
+public enum StoragePluginState
+{
+	ENABLED,
+	DISABLED,
+	UNINSTALLED
 }
