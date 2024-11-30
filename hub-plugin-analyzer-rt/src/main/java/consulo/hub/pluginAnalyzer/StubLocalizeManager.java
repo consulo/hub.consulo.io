8021830aabb9dbc55ee3a30ffb2d649cdc78648e
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-11-30 12:25:31 +0400

    fix compile

--- hub-plugin-analyzer-rt/src/main/java/consulo/hub/pluginAnalyzer/StubLocalizeManager.java
+++ hub-plugin-analyzer-rt/src/main/java/consulo/hub/pluginAnalyzer/StubLocalizeManager.java
@@ -5,80 +5,70 @@ import consulo.localize.LocalizeKey;
 import consulo.localize.LocalizeManager;
 import consulo.localize.LocalizeManagerListener;
 import consulo.localize.LocalizeValue;
-
 import jakarta.annotation.Nonnull;
+
 import java.util.Locale;
+import java.util.Map;
 import java.util.Set;
 
 /**
  * @author VISTALL
  * @since 06/05/2023
  */
-public class StubLocalizeManager extends LocalizeManager
-{
-	@Nonnull
-	@Override
-	public LocalizeValue fromStringKey(@Nonnull String s)
-	{
-		return LocalizeValue.of(s);
-	}
+public class StubLocalizeManager extends LocalizeManager {
+    @Nonnull
+    @Override
+    public LocalizeValue fromStringKey(@Nonnull String s) {
+        return LocalizeValue.of(s);
+    }
 
-	@Nonnull
-	@Override
-	public String getUnformattedText(@Nonnull LocalizeKey localizeKey)
-	{
-		return localizeKey.getKey();
-	}
+    @Nonnull
+    @Override
+    public Map.Entry<Locale, String> getUnformattedText(@Nonnull LocalizeKey localizeKey) {
+        return Map.entry(Locale.US, "");
+    }
 
-	@Nonnull
-	@Override
-	public Locale parseLocale(@Nonnull String s)
-	{
-		return Locale.ROOT;
-	}
+    @Nonnull
+    @Override
+    public Locale parseLocale(@Nonnull String s) {
+        return Locale.ROOT;
+    }
 
-	@Override
-	public void setLocale(@Nonnull Locale locale, boolean b)
-	{
+    @Override
+    public void setLocale(@Nonnull Locale locale, boolean b) {
 
-	}
+    }
 
-	@Nonnull
-	@Override
-	public Locale getLocale()
-	{
-		return Locale.ROOT;
-	}
+    @Nonnull
+    @Override
+    public Locale getLocale() {
+        return Locale.ROOT;
+    }
 
-	@Override
-	public boolean isDefaultLocale()
-	{
-		return true;
-	}
+    @Override
+    public boolean isDefaultLocale() {
+        return true;
+    }
 
-	@Nonnull
-	@Override
-	public Set<Locale> getAvaliableLocales()
-	{
-		return Set.of();
-	}
+    @Nonnull
+    @Override
+    public Set<Locale> getAvaliableLocales() {
+        return Set.of();
+    }
 
-	@Override
-	public void addListener(@Nonnull LocalizeManagerListener localizeManagerListener, @Nonnull Disposable disposable)
-	{
+    @Override
+    public void addListener(@Nonnull LocalizeManagerListener localizeManagerListener, @Nonnull Disposable disposable) {
 
-	}
+    }
 
-	@Override
-	public long getModificationCount()
-	{
-		return 0;
-	}
+    @Override
+    public long getModificationCount() {
+        return 0;
+    }
 
-	@Nonnull
-	@Override
-	public String formatText(String s, Object... objects)
-	{
-		return s;
-	}
+    @Nonnull
+    @Override
+    public String formatText(String s, Locale locale, Object[] objects) {
+        return s;
+    }
 }
