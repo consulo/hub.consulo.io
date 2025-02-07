c693fcf44010e9a2611aeb8ca065ee25dce5e413
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-02-07 18:11:29 +0400

    add riscv64 and loong64 builds, also unify ids of linux/windows

--- hub-backend/src/main/java/consulo/hub/backend/repository/controller/PluginChannelRestController.java
+++ hub-backend/src/main/java/consulo/hub/backend/repository/controller/PluginChannelRestController.java
@@ -32,247 +32,209 @@ import java.util.Objects;
  * @since 24-Sep-16
  */
 @RestController
-public class PluginChannelRestController
-{
-	@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
-	private static class NotAuthorizedException extends RuntimeException
-	{
-	}
+public class PluginChannelRestController {
+    @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+    private static class NotAuthorizedException extends RuntimeException {
+    }
 
-	private final RepositoryChannelsService myPluginChannelsService;
-	private final PluginDeployService myPluginDeployService;
-	private final PluginStatisticsService myPluginStatisticsService;
-	private final ObjectMapper myObjectMapper;
+    private final RepositoryChannelsService myPluginChannelsService;
+    private final PluginDeployService myPluginDeployService;
+    private final PluginStatisticsService myPluginStatisticsService;
+    private final ObjectMapper myObjectMapper;
 
-	@Autowired
-	public PluginChannelRestController(@Nonnull RepositoryChannelsService pluginChannelsService,
-									   @Nonnull PluginDeployService pluginDeployService,
-									   @Nonnull PluginStatisticsService pluginStatisticsService,
-									   ObjectMapper objectMapper)
-	{
-		myPluginChannelsService = pluginChannelsService;
-		myPluginDeployService = pluginDeployService;
-		myPluginStatisticsService = pluginStatisticsService;
-		myObjectMapper = objectMapper;
-	}
+    @Autowired
+    public PluginChannelRestController(@Nonnull RepositoryChannelsService pluginChannelsService,
+                                       @Nonnull PluginDeployService pluginDeployService,
+                                       @Nonnull PluginStatisticsService pluginStatisticsService,
+                                       ObjectMapper objectMapper) {
+        myPluginChannelsService = pluginChannelsService;
+        myPluginDeployService = pluginDeployService;
+        myPluginStatisticsService = pluginStatisticsService;
+        myObjectMapper = objectMapper;
+    }
 
-	// api methods
+    // api methods
 
-	@RequestMapping("/api/repository/download")
-	public ResponseEntity<?> download(@RequestParam("channel") PluginChannel channel,
-									  @RequestParam("platformVersion") String platformVersion,
-									  @Deprecated @RequestParam(value = "pluginId", required = false) final String pluginId,
-									  @RequestParam(value = "id", required = false /* TODO [VISTALL] remove it after dropping 'pluginId' parameter*/) final String id,
-									  @RequestParam(value = "noTracking", defaultValue = "false", required = false) boolean noTracking,
-									  @RequestParam(value = "platformBuildSelect", defaultValue = "false", required = false) boolean platformBuildSelect,
-									  @RequestParam(value = "zip", defaultValue = "false", required = false) boolean zip,
-									  @RequestParam(value = "viaUpdate", defaultValue = "false", required = false) boolean viaUpdate,
-									  @RequestParam(value = "noRedirect", defaultValue = "false", required = false) boolean noRedirect,
-									  @RequestParam(value = "version", required = false) String version)
-	{
-		if(id == null && pluginId == null)
-		{
-			throw new IllegalArgumentException("'id' is null");
-		}
+    @RequestMapping("/api/repository/download")
+    public ResponseEntity<?> download(@RequestParam("channel") PluginChannel channel,
+                                      @RequestParam("platformVersion") String platformVersion,
+                                      @Deprecated @RequestParam(value = "pluginId", required = false) final String pluginId,
+                                      @RequestParam(value = "id", required = false /* TODO [VISTALL] remove it after dropping 'pluginId' parameter*/) final String id,
+                                      @RequestParam(value = "noTracking", defaultValue = "false", required = false) boolean noTracking,
+                                      @RequestParam(value = "platformBuildSelect", defaultValue = "false", required = false) boolean platformBuildSelect,
+                                      @RequestParam(value = "zip", defaultValue = "false", required = false) boolean zip,
+                                      @RequestParam(value = "viaUpdate", defaultValue = "false", required = false) boolean viaUpdate,
+                                      @RequestParam(value = "noRedirect", defaultValue = "false", required = false) boolean noRedirect,
+                                      @RequestParam(value = "version", required = false) String version) {
+        if (id == null && pluginId == null) {
+            throw new IllegalArgumentException("'id' is null");
+        }
 
-		String idValue = id;
-		if(pluginId != null)
-		{
-			idValue = pluginId;
-		}
+        String idValue = id;
+        if (pluginId != null) {
+            idValue = pluginId;
+        }
 
-		RepositoryChannelStore channelService = myPluginChannelsService.getRepositoryByChannel(channel);
+        RepositoryChannelStore channelService = myPluginChannelsService.getRepositoryByChannel(channel);
 
-		String idNew = idValue;
-		if(zip)
-		{
-			idNew = idValue + "-zip";
-		}
+        String idNew = idValue;
+        if (zip) {
+            idNew = idValue + "-zip";
+        }
 
-		// FIXME [VISTALL] remap to new platform id - remove later
-		PlatformNodeDesc newPlatformDesc = PlatformNodeDesc.findByOldId(idNew);
-		if(newPlatformDesc != null)
-		{
-			idNew = newPlatformDesc.id();
-		}
+        // FIXME [VISTALL] remap to new platform id - remove later
+        PlatformNodeDesc newPlatformDesc = PlatformNodeDesc.findByOldId(idNew);
+        if (newPlatformDesc != null) {
+            idNew = newPlatformDesc.id();
+        }
 
-		PluginNode select = channelService.select(platformVersion, idNew, version, platformBuildSelect);
-		if(select == null)
-		{
-			idNew = idValue;
-			select = channelService.select(platformVersion, idNew, version, platformBuildSelect);
-		}
+        PluginNode select = channelService.select(platformVersion, idNew, version, platformBuildSelect);
+        if (select == null) {
+            idNew = idValue;
+            select = channelService.select(platformVersion, idNew, version, platformBuildSelect);
+        }
 
-		if(select == null)
-		{
-			return ResponseEntity.notFound().build();
-		}
+        if (select == null) {
+            return ResponseEntity.notFound().build();
+        }
 
-		if(!noTracking)
-		{
-			myPluginStatisticsService.increaseDownload(idNew, channel, select.version, platformVersion, viaUpdate);
-		}
+        if (!noTracking) {
+            myPluginStatisticsService.increaseDownload(idNew, channel, select.version, platformVersion, viaUpdate);
+        }
 
-		if(!noRedirect)
-		{
-			String[] downloadUrls = select.downloadUrls;
-			if(downloadUrls != null && downloadUrls.length > 0)
-			{
-				String downloadUrl = downloadUrls[0];
-				try
-				{
-					return ResponseEntity.status(HttpStatus.FOUND).location(new URI(downloadUrl)).build();
-				}
-				catch(URISyntaxException ignored)
-				{
-				}
-			}
-		}
+        if (!noRedirect) {
+            String[] downloadUrls = select.downloadUrls;
+            if (downloadUrls != null && downloadUrls.length > 0) {
+                String downloadUrl = downloadUrls[0];
+                try {
+                    return ResponseEntity.status(HttpStatus.FOUND).location(new URI(downloadUrl)).build();
+                }
+                catch (URISyntaxException ignored) {
+                }
+            }
+        }
 
-		File targetFile = select.targetFile;
-		if(targetFile != null)
-		{
-			return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + targetFile.getName() + "\"").body(new FileSystemResource(targetFile));
-		}
-		else
-		{
-			Path targetPath = Objects.requireNonNull(select.targetPath);
-			return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + targetPath.getFileName() + "\"").body(new FileSystemResource(targetPath));
-		}
-	}
+        File targetFile = select.targetFile;
+        if (targetFile != null) {
+            return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + targetFile.getName() + "\"").body(new FileSystemResource(targetFile));
+        }
+        else {
+            Path targetPath = Objects.requireNonNull(select.targetPath);
+            return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + targetPath.getFileName() + "\"").body(new FileSystemResource(targetPath));
+        }
+    }
 
-	@RequestMapping(value = "/api/repository/platformDeploy", method = RequestMethod.POST)
-	public PluginNode platformDeploy(@RequestParam("channel") PluginChannel channel,
-									 @RequestParam("file") MultipartFile file,
-									 @RequestParam(value = "history", required = false) MultipartFile history,
-									 @RequestParam(value = "github", required = false) MultipartFile github,
-									 @RequestParam("platformVersion") int platformVersion,
-									 @AuthenticationPrincipal UserAccount userAccount) throws Exception
-	{
-		if(!hasRole(userAccount, Roles.ROLE_SUPERDEPLOYER))
-		{
-			throw new NotAuthorizedException();
-		}
+    @RequestMapping(value = "/api/repository/platformDeploy", method = RequestMethod.POST)
+    public PluginNode platformDeploy(@RequestParam("channel") PluginChannel channel,
+                                     @RequestParam("file") MultipartFile file,
+                                     @RequestParam(value = "history", required = false) MultipartFile history,
+                                     @RequestParam(value = "github", required = false) MultipartFile github,
+                                     @RequestParam("platformVersion") int platformVersion,
+                                     @AuthenticationPrincipal UserAccount userAccount) throws Exception {
+        if (!hasRole(userAccount, Roles.ROLE_SUPERDEPLOYER)) {
+            throw new NotAuthorizedException();
+        }
 
-		RestPluginGithubInfo pluginGithubInfo = null;
-		if(github != null)
-		{
-			pluginGithubInfo = myObjectMapper.readValue(github.getBytes(), RestPluginGithubInfo.class);
-		}
+        RestPluginGithubInfo pluginGithubInfo = null;
+        if (github != null) {
+            pluginGithubInfo = myObjectMapper.readValue(github.getBytes(), RestPluginGithubInfo.class);
+        }
 
-		return myPluginDeployService.deployPlatform(channel, pluginGithubInfo, platformVersion, file, history);
-	}
+        return myPluginDeployService.deployPlatform(channel, pluginGithubInfo, platformVersion, file, history);
+    }
 
-	@RequestMapping(value = "/api/repository/pluginDeploy", method = RequestMethod.POST)
-	public PluginNode pluginDeploy(@RequestParam("channel") PluginChannel channel,
-								   @RequestParam("file") MultipartFile file,
-								   @RequestParam(value = "history", required = false) MultipartFile history,
-								   @RequestParam(value = "github", required = false) MultipartFile github,
-								   @AuthenticationPrincipal UserAccount userAccount) throws Exception
-	{
-		if(!hasRole(userAccount, Roles.ROLE_SUPERDEPLOYER))
-		{
-			throw new NotAuthorizedException();
-		}
+    @RequestMapping(value = "/api/repository/pluginDeploy", method = RequestMethod.POST)
+    public PluginNode pluginDeploy(@RequestParam("channel") PluginChannel channel,
+                                   @RequestParam("file") MultipartFile file,
+                                   @RequestParam(value = "history", required = false) MultipartFile history,
+                                   @RequestParam(value = "github", required = false) MultipartFile github,
+                                   @AuthenticationPrincipal UserAccount userAccount) throws Exception {
+        if (!hasRole(userAccount, Roles.ROLE_SUPERDEPLOYER)) {
+            throw new NotAuthorizedException();
+        }
 
-		RestPluginGithubInfo pluginGithubInfo = null;
-		if(github != null)
-		{
-			pluginGithubInfo = myObjectMapper.readValue(github.getBytes(), RestPluginGithubInfo.class);
-		}
-		return myPluginDeployService.deployPlugin(channel, () -> history == null ? null : history.getInputStream(), file::getInputStream, pluginGithubInfo);
-	}
+        RestPluginGithubInfo pluginGithubInfo = null;
+        if (github != null) {
+            pluginGithubInfo = myObjectMapper.readValue(github.getBytes(), RestPluginGithubInfo.class);
+        }
+        return myPluginDeployService.deployPlugin(channel, () -> history == null ? null : history.getInputStream(), file::getInputStream, pluginGithubInfo);
+    }
 
-	private static boolean hasRole(UserAccount userAccount, String role)
-	{
-		return userAccount.getAuthorities().contains(new SimpleGrantedAuthority(role));
-	}
+    private static boolean hasRole(UserAccount userAccount, String role) {
+        return userAccount.getAuthorities().contains(new SimpleGrantedAuthority(role));
+    }
 
-	@RequestMapping("/api/repository/list")
-	public List<PluginNode> list(@RequestParam("channel") PluginChannel channel,
-								 @RequestParam("platformVersion") String platformVersion,
-								 @RequestParam(value = "addObsoletePlatforms", defaultValue = "true", required = false) boolean addObsoletePlatforms,
-								 @RequestParam(value = "platformBuildSelect", defaultValue = "false", required = false) boolean platformBuildSelect)
-	{
-		RepositoryChannelStore channelService = myPluginChannelsService.getRepositoryByChannel(channel);
+    @RequestMapping("/api/repository/list")
+    public List<PluginNode> list(@RequestParam("channel") PluginChannel channel,
+                                 @RequestParam("platformVersion") String platformVersion,
+                                 @RequestParam(value = "addObsoletePlatforms", defaultValue = "true", required = false) boolean addObsoletePlatforms,
+                                 @RequestParam(value = "platformBuildSelect", defaultValue = "false", required = false) boolean platformBuildSelect) {
+        RepositoryChannelStore channelService = myPluginChannelsService.getRepositoryByChannel(channel);
 
-		ArrayList<PluginNode> result = channelService.select(myPluginStatisticsService, platformVersion, platformBuildSelect);
+        ArrayList<PluginNode> result = channelService.select(myPluginStatisticsService, platformVersion, platformBuildSelect);
 
-		// TODO we put old style platform ids
-		if(addObsoletePlatforms)
-		{
-			Collection<PlatformNodeDesc> values = PlatformNodeDesc.values();
+        // TODO we put old style platform ids
+        if (addObsoletePlatforms) {
+            Collection<PlatformNodeDesc> values = PlatformNodeDesc.values();
 
-			result.ensureCapacity(result.size() + values.size());
+            result.ensureCapacity(result.size() + values.size());
 
-			List<PluginNode> join = new ArrayList<>(values.size());
+            List<PluginNode> join = new ArrayList<>(values.size());
 
-			for(PluginNode node : result)
-			{
-				PlatformNodeDesc nodeDesc = PlatformNodeDesc.getNode(node.id);
-				if(nodeDesc == null)
-				{
-					continue;
-				}
+            for (PluginNode node : result) {
+                PlatformNodeDesc nodeDesc = PlatformNodeDesc.getNode(node.id);
+                if (nodeDesc == null) {
+                    continue;
+                }
 
-				String oldId = nodeDesc.oldId();
-				if(oldId == null)
-				{
-					continue;
-				}
+                for (String oldId : nodeDesc.oldIds()) {
+                    PluginNode oldNode = node.clone();
+                    oldNode.id = oldId;
+                    oldNode.obsolete = true;
+                    join.add(oldNode);
+                }
+            }
 
-				PluginNode oldNode = node.clone();
-				oldNode.id = oldId;
-				oldNode.obsolete = true;
-				join.add(oldNode);
-			}
+            result.addAll(join);
+        }
 
-			result.addAll(join);
-		}
+        return result;
+    }
 
-		return result;
-	}
+    @RequestMapping("/api/repository/info")
+    public ResponseEntity<PluginNode> info(@RequestParam("channel") PluginChannel channel,
+                                           @RequestParam("platformVersion") String platformVersion,
+                                           @RequestParam("id") final String id,
+                                           @RequestParam(value = "zip", defaultValue = "false", required = false) boolean zip,
+                                           @RequestParam(value = "version") String version) {
+        RepositoryChannelStore channelService = myPluginChannelsService.getRepositoryByChannel(channel);
 
-	@RequestMapping("/api/repository/info")
-	public ResponseEntity<PluginNode> info(@RequestParam("channel") PluginChannel channel,
-										   @RequestParam("platformVersion") String platformVersion,
-										   @RequestParam("id") final String id,
-										   @RequestParam(value = "zip", defaultValue = "false", required = false) boolean zip,
-										   @RequestParam(value = "version") String version)
-	{
-		RepositoryChannelStore channelService = myPluginChannelsService.getRepositoryByChannel(channel);
+        String idNew = id;
+        if (zip) {
+            idNew = id + "-zip";
+        }
 
-		String idNew = id;
-		if(zip)
-		{
-			idNew = id + "-zip";
-		}
+        // FIXME [VISTALL] remap to new platform id - remove later
+        PlatformNodeDesc newPlatformDesc = PlatformNodeDesc.findByOldId(idNew);
+        if (newPlatformDesc != null) {
+            idNew = newPlatformDesc.id();
+        }
 
-		// FIXME [VISTALL] remap to new platform id - remove later
-		PlatformNodeDesc newPlatformDesc = PlatformNodeDesc.findByOldId(idNew);
-		if(newPlatformDesc != null)
-		{
-			idNew = newPlatformDesc.id();
-		}
+        PluginNode select = channelService.select(platformVersion, idNew, version, true);
+        if (select == null) {
+            idNew = id;
+            select = channelService.select(platformVersion, idNew, version, true);
+        }
 
-		PluginNode select = channelService.select(platformVersion, idNew, version, true);
-		if(select == null)
-		{
-			idNew = id;
-			select = channelService.select(platformVersion, idNew, version, true);
-		}
+        if (select == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
 
-		if(select == null)
-		{
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-		}
+        return ResponseEntity.ok(select.clone());
+    }
 
-		return ResponseEntity.ok(select.clone());
-	}
-
-	@RequestMapping("/api/repository/selectChannel")
-	public ResponseEntity<PluginChannelSelect> selectChannel(@RequestParam("platformId") final String platformId, @RequestParam("platformVersion") String platformVersion)
-	{
-		return ResponseEntity.ok(new PluginChannelSelect());
-	}
+    @RequestMapping("/api/repository/selectChannel")
+    public ResponseEntity<PluginChannelSelect> selectChannel(@RequestParam("platformId") final String platformId, @RequestParam("platformVersion") String platformVersion) {
+        return ResponseEntity.ok(new PluginChannelSelect());
+    }
 }
--- hub-shared/src/main/java/consulo/hub/shared/repository/util/PlatformNodeDesc.java
+++ hub-shared/src/main/java/consulo/hub/shared/repository/util/PlatformNodeDesc.java
@@ -3,69 +3,63 @@ package consulo.hub.shared.repository.util;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author VISTALL
  * @since 22/05/2023
  */
-public record PlatformNodeDesc(String id, String name, String oldId, String ext)
-{
-	private static Map<String, PlatformNodeDesc> ourNodes = new HashMap<>();
-	private static Map<String, PlatformNodeDesc> ourOldMapping = new HashMap<>();
+public record PlatformNodeDesc(String id, String name, Set<String> oldIds, String ext) {
+    private static Map<String, PlatformNodeDesc> ourNodes = new HashMap<>();
+    private static Map<String, PlatformNodeDesc> ourOldMapping = new HashMap<>();
 
-	static
-	{
-		add(new PlatformNodeDesc("consulo.dist.linux.no.jre", "Platform (Linux, without JRE)", "consulo-linux-no-jre", "tar.gz"));
-		add(new PlatformNodeDesc("consulo.dist.linux", "Platform (Linux, with JRE x86)", "consulo-linux", "tar.gz"));
-		add(new PlatformNodeDesc("consulo.dist.linux64", "Platform (Linux, with JRE x64)", "consulo-linux64", "tar.gz"));
-		add(new PlatformNodeDesc("consulo.dist.linuxA64", "Platform (Linux, with JRE ARM64)", null, "tar.gz"));
+    static {
+        add(new PlatformNodeDesc("consulo.dist.linux.no.jre", "Platform (Linux, without JRE)", Set.of("consulo-linux-no-jre"), "tar.gz"));
+        add(new PlatformNodeDesc("consulo.dist.linux", "Platform (Linux, with JRE x86)", Set.of("consulo-linux"), "tar.gz"));
+        add(new PlatformNodeDesc("consulo.dist.linux64", "Platform (Linux, with JRE x64)", Set.of("consulo-linux64"), "tar.gz"));
+        add(new PlatformNodeDesc("consulo.dist.linux.aarch64", "Platform (Linux, with JRE ARM64)", Set.of("consulo.dist.linuxA64"), "tar.gz"));
+        add(new PlatformNodeDesc("consulo.dist.linux.riscv64", "Platform (Linux, with JRE RISCV64)", Set.of(), "tar.gz"));
+        add(new PlatformNodeDesc("consulo.dist.linux.loong64", "Platform (Linux, with JRE LOONGARCH64)", Set.of(), "tar.gz"));
 
-		add(new PlatformNodeDesc("consulo.dist.mac64.no.jre", "Platform (macOS x64, without JRE)", "consulo-mac-no-jre", "tar.gz"));
-		add(new PlatformNodeDesc("consulo.dist.mac64", "Platform (macOS x64, with JRE)", "consulo-mac64", "tar.gz"));
+        add(new PlatformNodeDesc("consulo.dist.mac64.no.jre", "Platform (macOS x64, without JRE)", Set.of("consulo-mac-no-jre"), "tar.gz"));
+        add(new PlatformNodeDesc("consulo.dist.mac64", "Platform (macOS x64, with JRE)", Set.of("consulo-mac64"), "tar.gz"));
 
-		add(new PlatformNodeDesc("consulo.dist.macA64.no.jre", "Platform (macOS ARM64, without JRE)", null, "tar.gz"));
-		add(new PlatformNodeDesc("consulo.dist.macA64", "Platform (macOS ARM64, with JRE)", null, "tar.gz"));
+        add(new PlatformNodeDesc("consulo.dist.macA64.no.jre", "Platform (macOS ARM64, without JRE)", Set.of(), "tar.gz"));
+        add(new PlatformNodeDesc("consulo.dist.macA64", "Platform (macOS ARM64, with JRE)", Set.of(), "tar.gz"));
 
-		add(new PlatformNodeDesc("consulo.dist.windows.no.jre", "Platform (Windows, without JRE)", "consulo-win-no-jre", "tar.gz"));
-		add(new PlatformNodeDesc("consulo.dist.windows.no.jre.zip", "Platform (Windows, without JRE, zip archive)", "consulo-win-no-jre-zip", "zip"));
+        add(new PlatformNodeDesc("consulo.dist.windows.no.jre", "Platform (Windows, without JRE)", Set.of("consulo-win-no-jre"), "tar.gz"));
+        add(new PlatformNodeDesc("consulo.dist.windows.no.jre.zip", "Platform (Windows, without JRE, zip archive)", Set.of("consulo-win-no-jre-zip"), "zip"));
 
-		add(new PlatformNodeDesc("consulo.dist.windows", "Platform (Windows, with JRE x86)", "consulo-win", "tar.gz"));
-		add(new PlatformNodeDesc("consulo.dist.windows.zip", "Platform (Windows, zip)", "consulo-win-zip", "zip"));
+        add(new PlatformNodeDesc("consulo.dist.windows", "Platform (Windows, with JRE x86)", Set.of("consulo-win"), "tar.gz"));
+        add(new PlatformNodeDesc("consulo.dist.windows.zip", "Platform (Windows, zip)", Set.of("consulo-win-zip"), "zip"));
 
-		add(new PlatformNodeDesc("consulo.dist.windows64", "Platform (Windows, with JRE x64)", "consulo-win64", "tar.gz"));
-		add(new PlatformNodeDesc("consulo.dist.windows64.installer", "Platform (Windows, with JRE x64, installer)", null, "exe"));
-		add(new PlatformNodeDesc("consulo.dist.windows64.zip", "Platform (Windows, with JRE x64, zip)", "consulo-win64-zip", "zip"));
+        add(new PlatformNodeDesc("consulo.dist.windows64", "Platform (Windows, with JRE x64)", Set.of("consulo-win64"), "tar.gz"));
+        add(new PlatformNodeDesc("consulo.dist.windows64.installer", "Platform (Windows, with JRE x64, installer)", Set.of(), "exe"));
+        add(new PlatformNodeDesc("consulo.dist.windows64.zip", "Platform (Windows, with JRE x64, zip)", Set.of("consulo-win64-zip"), "zip"));
 
-		add(new PlatformNodeDesc("consulo.dist.windowsA64", "Platform (Windows, with JRE ARM64)", "consulo-winA64", "tar.gz"));
-		add(new PlatformNodeDesc("consulo.dist.windowsA64.zip", "Platform (Windows, with JRE ARM64, zip)", "consulo-winA64-zip", "zip"));
-	}
+        add(new PlatformNodeDesc("consulo.dist.windows.aarch64", "Platform (Windows, with JRE ARM64)", Set.of("consulo-winA64", "consulo.dist.windowsA64"), "tar.gz"));
+        add(new PlatformNodeDesc("consulo.dist.windows.aarch64.zip", "Platform (Windows, with JRE ARM64, zip)", Set.of("consulo-winA64-zip", "consulo.dist.windowsA64.zip"), "zip"));
+    }
 
-	public static PlatformNodeDesc getNode(String id)
-	{
-		return ourNodes.get(id);
-	}
+    public static PlatformNodeDesc getNode(String id) {
+        return ourNodes.get(id);
+    }
 
-	public static Collection<PlatformNodeDesc> values()
-	{
-		return ourNodes.values();
-	}
+    public static Collection<PlatformNodeDesc> values() {
+        return ourNodes.values();
+    }
 
-	public static PlatformNodeDesc findByOldId(String oldId)
-	{
-		return ourOldMapping.get(oldId);
-	}
+    public static PlatformNodeDesc findByOldId(String oldId) {
+        return ourOldMapping.get(oldId);
+    }
 
-	private static void add(PlatformNodeDesc node)
-	{
-		if(ourNodes.put(node.id(), node) != null)
-		{
-			throw new IllegalArgumentException("Duplicate " + node.id());
-		}
+    private static void add(PlatformNodeDesc node) {
+        if (ourNodes.put(node.id(), node) != null) {
+            throw new IllegalArgumentException("Duplicate " + node.id());
+        }
 
-		String oldId = node.oldId();
-		if(oldId != null)
-		{
-			ourOldMapping.put(oldId, node);
-		}
-	}
+        for (String oldId : node.oldIds()) {
+            ourOldMapping.put(oldId, node);
+        }
+    }
 }
