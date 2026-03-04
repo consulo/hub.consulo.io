8f1c730ad05077bd2aaf28affdcb99a2a932acf1
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-03-04 17:46:16 +0200

    add valhalla channel again

--- hub-shared/src/main/java/consulo/hub/shared/repository/PluginChannel.java
+++ hub-shared/src/main/java/consulo/hub/shared/repository/PluginChannel.java
@@ -4,10 +4,12 @@ package consulo.hub.shared.repository;
  * @author VISTALL
  * @since 28-Aug-16
  */
-public enum PluginChannel
-{
-	release, // every month
-	beta,    // every week
-	alpha,   // every day
-	nightly, // every commit
+public enum PluginChannel {
+    release, // every month
+    beta,    // every week
+    alpha,   // every day
+    nightly, // every commit
+    // used for 3-SNAPSHOT - module system, refactoring
+    // used for 4-SNAPSHOT - rewrite lock system
+    valhalla
 }
